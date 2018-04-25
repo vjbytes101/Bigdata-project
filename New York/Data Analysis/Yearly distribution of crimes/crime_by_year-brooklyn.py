e3cb21e1c1d49de558cd1489736da34095ba2719
vjbytes101 <vj.yadav91@yahoo.com>
2018-04-24 23:19:55 -0400

    'datayearlyanalysis'

---
+++ New York/Data Analysis/Yearly distribution of crimes/crime_by_year-brooklyn.py
@@ -0,0 +1,13 @@
+import sys
+import csv
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+
+spark = SparkSession.builder.appName("Python Spark SQL basic example").config("spark.some.config.option", "some-value").getOrCreate()
+df_parking = spark.read.format("csv").option("header", "true").load("/user/edureka_123073/result.csv")
+temp = df_parking.withColumn("tx_date", to_date(unix_timestamp(df_parking['RPT_DT'], "mm/dd/yyyy").cast("timestamp")))
+df=temp
+df.createOrReplaceTempView("df")
+result = spark.sql("Select year(tx_date) as year_val, count(year(tx_date)) as count_val from df where tx_date !='tx_date' and BORO_NM = 'BROOKLYN' group by year(tx_date) ORDER BY year(tx_date)")
+result.select(format_string('%s\t%d', result.year_val,result.count_val)).write.save("/Output/Analysis/crime_by_year_brooklyn.csv",format="csv")
