a43465ba6dd95c6d05dfb361d226f50ae51e4199
vjbytes101 <vj.yadav91@yahoo.com>
2018-04-28 23:47:41 -0400

    'analysis'

---
+++ New York/Data Analysis/crime_type/crime_type.py
@@ -0,0 +1,10 @@
+import sys
+import csv
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.appName("Python Spark SQL basic example").config("spark.some.config.option", "some-value").getOrCreate()
+df_parking = spark.read.format("csv").option("header", "true").load("/user/edureka_123073/result.csv")
+df_parking.createOrReplaceTempView('crime')
+result = spark.sql("Select OFNS_DESC as ofns,count(OFNS_DESC) as count_val from crime group by OFNS_DESC ORDER BY count(OFNS_DESC) desc limit 10")
+result.select(format_string('%s\t%d', result.ofns,result.count_val)).write.save("/Output/Analysis/crime_by_type.csv",format="csv")
